b243463d34a02fa25b4ec0c422e80e4582251394
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-02-26 23:24:58 +0530

    admin sidebar ui is completed

--- src/components/admin/sidebar/sidebar.tsx
+++ src/components/admin/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Architects_Daughter } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
 import {
   Sidebar as ReactProSidebar,
   Menu,
@@ -12,6 +12,8 @@ import { BiSolidCategory } from "react-icons/bi";
 import { FaBookOpen, FaHome, FaHotel } from "react-icons/fa";
 import { LuLogOut } from "react-icons/lu";
 import { MdOutlineDataUsage } from "react-icons/md";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const ArchitectsDaughter = Architects_Daughter({
   weight: "400",
@@ -20,6 +22,9 @@ const ArchitectsDaughter = Architects_Daughter({
 });
 
 const Sidebar = () => {
+  const router = useRouter();
+  const [selectedItem, setSelectedItem] = useState("/admin/dashborad");
+
   const menuItems = [
     {
       label: "Dashboard",
@@ -48,9 +53,72 @@ const Sidebar = () => {
     },
   ];
 
+  const handleItemClick = (link: string) => {
+    setSelectedItem(link);
+    router.push(link);
+  };
+
   return (
-    <div className="min-h[100vh] overflow-hidden">
-      <ReactProSidebar className="h-full overflow-hidden"></ReactProSidebar>
+    <div className="min-h-[100vh] overflow-hidden">
+      <ReactProSidebar
+        className="h-full overflow-hidden"
+        rootStyles={{
+          [`/${sidebarClasses.container}`]: {
+            backgroundColor: "#ffffff",
+            "&:hover": {
+              backgroundColor: "#ffffff",
+            },
+          },
+        }}
+      >
+        <Menu
+          className="h-[100vh] max-h-[100vh] text-black overflow-hidden"
+          menuItemStyles={{
+            button: ({ level, active }) => {
+              return {
+                backgroundColor: active ? "#0e1428" : "#ffffff",
+                color: active ? "#ffffff" : "#000000",
+                "&:hover": {
+                  backgroundColor: active ? "#0e1428" : "#0e1428",
+                  color: active ? "#ffffff" : "#ffffff",
+                },
+              };
+            },
+          }}
+        >
+          <div className="flex items-center justify-center my-10 flex-col">
+            <Image
+              src="/logo.png"
+              alt="logo"
+              height={150}
+              width={150}
+              className="cursor-pointer"
+              onClick={() => router.push("/admin/dashboard")}
+            />
+            <span className="text-3xl uppercase font-medium italic">
+              <span className={ArchitectsDaughter.className}>ARKLYTE</span>
+            </span>
+          </div>
+          {menuItems.map((item, index) => (
+            <React.Fragment key={index}>
+              <MenuItem
+                icon={item.icons}
+                active={selectedItem === item.link}
+                onClick={() => handleItemClick(item.link)}
+              >
+                {item.label}
+              </MenuItem>
+            </React.Fragment>
+          ))}
+          <MenuItem
+            icon={<LuLogOut />}
+            active={selectedItem === "/admin/logout"}
+            onClick={() => handleItemClick("/admin/logout")}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </ReactProSidebar>
     </div>
   );
 };
